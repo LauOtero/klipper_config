cce6c588808a631895e0634ab249f838edc107f7
LauOtero <laureanootero@gmail.com>
2022-01-25 01:41:28 +0100

    Autocommit from 2022-01-25 01:41:28

--- backup_cfg_github.sh
+++ backup_cfg_github.sh
@@ -7,19 +7,7 @@
 ### Path to your config folder you want to backup
 config_folder=~/klipper_config
 
-### Path to your Klipper folder, by default that is '~/klipper'
-#klipper_folder=~/klipper
-
-### Path to your Moonraker folder, by default that is '~/moonraker'
-#moonraker_folder=~/moonraker
-
-### Path to your Mainsail folder, by default that is '~/mainsail'
-#mainsail_folder=~/mainsail
-
-### Path to your Fluidd folder, by default that is '~/fluidd'
-#fluidd_folder=~/fluidd
-
-#####################################################################
+####################################################################
 #####################################################################
 
 
@@ -33,28 +21,15 @@ grab_version(){
     m1="Klipper on commit: $klipper_commit"
     cd ..
   fi
-  if [ ! -z "$moonraker_folder" ]; then
-    cd "$moonraker_folder"
-    moonraker_commit=$(git rev-parse --short=7 HEAD)
-    m2="Moonraker on commit: $moonraker_commit"
-    cd ..
-  fi
-  if [ ! -z "$mainsail_folder" ]; then
-    mainsail_ver=$(head -n 1 $mainsail_folder/.version)
-    m3="Mainsail version: $mainsail_ver"
-  fi
-  if [ ! -z "$fluidd_folder" ]; then
-    fluidd_ver=$(head -n 1 $fluidd_folder/.version)
-    m4="Fluidd version: $fluidd_ver"
-  fi
 }
 
 push_config(){
   cd $config_folder
+  curl -H 'Authorization: token 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'
   git pull
   git add .
   current_date=$(date +"%Y-%m-%d %T")
-  git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
+  git commit -m "Autocommit from $current_date" -m "$m1"
   git push
 }
 
